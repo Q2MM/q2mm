e0726de7258fdbec7a805b10320cb2d512c020a9
Kevin Koh <jkoh@nd.edu>
2019-10-24 15:03:09 -0400

    sch_util.py description

--- q2mm/sch_util.py
+++ q2mm/sch_util.py
@@ -3,6 +3,12 @@ import schrodinger.structutils.analyze as ana
 import constants as co
 
 def interation234(filename):
+    """
+    Takes mae filename and export the information on 1-2,1-3,1-4 interacting atoms
+    This is needed to correctly assign the weights on the Hessian matrix
+    :param filename: .mae filename
+    :return: tuples of pair of atoms with 1-2,1-3,1-4 interaction
+    """
     st = next(struct.StructureReader(filename))
     bonds = ana.bond_iterator(st)
     angles = ana.angle_iterator(st)
@@ -19,6 +25,14 @@ def interation234(filename):
     return [int2,int3,int4]
 
 def wht(at1,at2,ints):
+    """
+    Assign weights for Hessian matrix given 1-2,1-3,1-4 interactions
+
+    :param at1: first atom
+    :param at2: second atom
+    :param ints: tuples of 1-2,1-3,1-4 interacting pair
+    :return: 0 for diagonal, assigned weight constant for all other
+    """
     apair = [at1,at2]
     int2, int3, int4 = ints
     if at1 == at2:
