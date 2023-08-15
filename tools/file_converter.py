1d70fe70d41ed376513111d861bf2f4acf09abf5
mmfarrugia <mfarrugi@nd.edu>
2023-08-15 11:45:17 -0400

    pre-test seminario amber fchk mol2 only

--- tools/file_converter.py
+++ tools/file_converter.py
@@ -13,9 +13,9 @@ def mae_to_mol2(infile,outfile):
     conv.ReadFile(mol, infile)
     conv.WriteFile(mol, outfile)
 
-def mae_to_mol2(infile,outfile):
+def fchk_to_mol2(infile,outfile):
     conv = ob.OBConversion()
-    conv.SetInAndOutFormats("mae","mol2")
+    conv.SetInAndOutFormats("fchk","mol2")
     mol = ob.OBMol()
     conv.ReadFile(mol, infile)
     conv.WriteFile(mol, outfile)
