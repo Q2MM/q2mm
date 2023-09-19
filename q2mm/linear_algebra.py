d9b5730a58529dddd791f8dda4b992c6bf171cc4
mmfarrugia <mikaelamariefarrugia@gmail.com>
2023-09-19 14:39:01 +0200

    ADD: linear_algebra tests, FIX: replace_neg_evals

--- q2mm/linear_algebra.py
+++ q2mm/linear_algebra.py
@@ -7,30 +7,32 @@ import os
 import sys
 
 import numpy as np
-import parmed
-
-from datatypes import AmberFF
 
 import logging
 import logging.config
-import constants as co
 
 
 # region Generalized
 
-def decompose(matrix:np.ndarray) -> (np.ndarray, np.ndarray):
+
+def decompose(matrix: np.ndarray) -> (np.ndarray, np.ndarray):
     """_summary_
 
     Args:
         matrix (np.ndarray): Matrix to decompose, matrix must be square.
 
     Returns:
-        (np.ndarray, np.ndarray): _description_
-    """    
+        (np.ndarray, np.ndarray): (eigenvalues, eigenvectors) where eigenvalues
+         is of shape (1,n) and eigenvectors is of shape (n,n) with n rows of
+         eigenvectors of length n.
+    """
     eigenvalues, eigenvectors = np.linalg.eigh(matrix)
     return eigenvalues, eigenvectors
 
-def replace_neg_eigenvalue(eigenvalues:np.ndarray, replace_with=1.0, zer_out_neg=False) -> np.ndarray:
+
+def replace_neg_eigenvalue(
+    eigenvalues: np.ndarray, replace_with=1.0, zer_out_neg=False
+) -> np.ndarray:
     """_summary_
 
     Args:
@@ -39,30 +41,34 @@ def replace_neg_eigenvalue(eigenvalues:np.ndarray, replace_with=1.0, zer_out_neg
 
     Returns:
         np.ndarray: _description_
-    """    
+    """
     neg_indices = np.argwhere([eval < 0 for eval in eigenvalues])
 
     if len(neg_indices) > 1:
-        print("more than one neg. eigenvalue: "+str(neg_indices))
+        print("more than one neg. eigenvalue: " + str(neg_indices))
         index_to_replace = np.argmin(eigenvalues)
-    else :
-        index_to_replace = neg_indices[0]
+    else:
+        index_to_replace = neg_indices[0][0]
     replaced_eigenvalues = eigenvalues
-    
-    #TODO: MF - Discussed with PO, decide if this should be implemented as it is not in current Q2MM
+
+    # TODO: MF - Discussed with PO, decide if this should be implemented as it is not in current Q2MM
     # for neg_index in neg_indices:
     #     if neg_index != index_to_replace:
     #         replaced_eigenvalues[neg_index] = 0
     if zer_out_neg:
         for neg_index in neg_indices:
-            replaced_eigenvalues[neg_index] = 0.00
-    replaced_eigenvalues[index_to_replace] = replace_with #TODO: MF determine if we stick to this method, what it depends on, etc
+            replaced_eigenvalues[neg_index[0]] = 0.00
+    replaced_eigenvalues[
+        index_to_replace
+    ] = replace_with  # TODO: MF determine if we stick to this method, what it depends on, etc
 
     return replaced_eigenvalues
+
 
 # endregion Generalized
 
 # region Hessian-specific
+
 
 def reform_hessian(eigenvalues: np.ndarray, eigenvectors: np.ndarray) -> np.ndarray:
     """_summary_
@@ -74,8 +80,9 @@ def reform_hessian(eigenvalues: np.ndarray, eigenvectors: np.ndarray) -> np.ndar
     Returns:
         np.ndarray: _description_
     """
-    reformed_hessian = (np.diag(eigenvalues).dot(np.transpose(eigenvectors)))
+    reformed_hessian = np.diag(eigenvalues).dot(np.transpose(eigenvectors))
     return reformed_hessian
+
 
 def invert_ts_curvature(hessian_matrix: np.ndarray) -> np.ndarray:
     """_summary_
@@ -85,12 +92,13 @@ def invert_ts_curvature(hessian_matrix: np.ndarray) -> np.ndarray:
 
     Returns:
         np.ndarray: _description_
-    """    
+    """
     eigenvalues, eigenvectors = decompose(hessian_matrix)
-    inv_curv_hessian = reform_hessian(replace_neg_eigenvalue(eigenvalues, zer_out_neg=True), eigenvectors)
-    
-    return inv_curv_hessian
+    inv_curv_hessian = reform_hessian(
+        replace_neg_eigenvalue(eigenvalues, zer_out_neg=True), eigenvectors
+    )
 
+    return inv_curv_hessian
 
 
 # endregion Hessian-specific
--- test/test_linear_algebra.py
+++ test/test_linear_algebra.py
@@ -1,31 +1,72 @@
 from __future__ import print_function
-import copy
-import logging
-import logging.config
+
 import os
+import sys
+
 import unittest
-import parmed
 import numpy as np
 
-import constants as co
-import datatypes
+src_dir = os.path.abspath("q2mm")
+sys.path.append(src_dir)
+import linear_algebra
 
 
-class MakeInput(object) :
-
-    def __init__(self) :
+class MakeHessian(object):
+    def __init__(self):
         self.out = []
 
-    def write(self, str) :
+    def write(self, str):
         self.out.append(str)
 
-    def __str__(self) :
+    def __str__(self):
         return "".join(self.out)
 
 
+example_sq3 = np.asarray([[-2, -4, 2], [-4, 1, 2], [2, 2, 5]])
+# Eigenvalues are sorted in ascending order to match the output from np.linalg.eigh
+# which is used by linear_algebra methods (decompose in particular)
+evals_sq3 = np.asarray([-5.51082, 3.659278, 5.851542])
+# Matrix rows must be ordered such that eigenvectors correspond to correct eigenvalues
+evec_sq3 = np.asarray(
+    [[-3.06513, -2.19028, 1], [2.82137, -3.49173, 1], [0.0770902, 0.348681, 1]]
+)
+evec_normd_sq3 = np.array([evec / np.linalg.norm(evec) for evec in evec_sq3])
+
+
 class TestLinearAlgebra(unittest.TestCase):
-
-    def test_loadstruct(self) :
-
-        struct = parmed.load_file(gro_filename, skip_bonds=True)
-        self.assertEqual(len(struct.atoms),41)
+    def test_replace_neg_eigenvalue(self):
+        repl_evals = linear_algebra.replace_neg_eigenvalue(evals_sq3)
+        np.testing.assert_allclose([1.0, evals_sq3[1], evals_sq3[2]], repl_evals)
+        multi_neg_evals = [
+            -5,
+            -2.45,
+            -1,
+            -0.2,
+            0.3,
+            0.459,
+            0.5,
+            1.8,
+            2.3,
+            4.6,
+            5,
+            9.87456,
+        ]
+        multi_neg_evals_repl = [
+            1.0,
+            0.0,
+            0.0,
+            0.0,
+            0.3,
+            0.459,
+            0.5,
+            1.8,
+            2.3,
+            4.6,
+            5,
+            9.87456,
+        ]
+        np.testing.assert_allclose(
+            multi_neg_evals_repl,
+            linear_algebra.replace_neg_eigenvalue(multi_neg_evals, zer_out_neg=True),
+            err_msg="Replaced eigenvalues do not match. Failed to replace excess negative values with zero.",
+        )
