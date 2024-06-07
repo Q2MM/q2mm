77b56704fe06012dc854d3308fec16c9a47e9632
mmfarrugia <mfarrugi@nd.edu>
2024-06-07 11:52:25 -0400

    ADD: plotting tools for parameterization ff_plotting.py in tools

---
+++ tools/ff_plotting.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+import os
+import matplotlib.pyplot as plt
+from pathlib import Path
+import numpy as np
+import sys
+from matplotlib import animation, rc
+from IPython.display import HTML, Image
+import seaborn
+import pickle
+import pandas as pd
+from numpy import copy
+from scipy import stats
+import itertools
+
+from sklearn.metrics import r2_score
+
+seaborn.set_theme(style="whitegrid")
+
+#src_dir = os.path.abspath("/home/mmfarrugia/repos/q2mm/q2mm")
+sys.path.append("/home/mfarrugi/repos/q2mm/rh-hybrid/schrodinger.ve/lib/python3.8/site-packages/q2mm-0.0.0-py3.8.egg")
+
+#from hybrid_optimizer import PSO_GA
+import q2mm.hybrid_optimizer as hybrid_optimizer
+from q2mm.hybrid_optimizer import PSO_DE
+from tools.plotters import plot_cost_history, plot_contour, plot_surface, plot_summary, Mesher, Designer
+
+bond_cols = ["param_type", "atom1", "atom2", "Equilibrium Value", "Force Constant", "Dipole Moment", "FF"]
+angle_cols = ["param_type", "atom1", "atom2", "atom3", "Equilibrium Value", "Force Constant", "FF"]
+
+def plot_history(base_direc:str, directories:list, title:str, cycle_iter_length:int, starting_score:float) -> list :
+    fig, ax = plt.subplots(1, 2, figsize=(24, 8))
+    fig.suptitle(title)
+    ax[0].set_title('Score Diversity Throughout Parameterization')
+    ax[1].set_title('Loss, aka Best Score Throughout Parameterization')
+
+    ax[0].axhline(y=starting_score, color='gray')
+    ax[1].axhline(y=starting_score, color='gray')
+
+    final_scores = []
+
+    for directory in directories:
+
+        swarm_history_file = open(os.path.join(base_direc, directory, 'hybrid_opt_history.bin'), 'rb')
+        swarm_history = pickle.load(swarm_history_file)
+        swarm_history_file.close()
+        num_iters = len(swarm_history['Y'])
+        num_ffs = len(swarm_history['Y'][0])
+        color = next(ax[0]._get_lines.prop_cycler)['color']
+        Y_history = pd.DataFrame(np.array(swarm_history['Y']).reshape((num_iters, num_ffs)))
+        ax[0].plot(Y_history.index, Y_history.values, '.', color=color)
+        loss = Y_history.min(axis=1).cummin()
+        loss.plot(kind='line', ax=ax[1], color=color, label='Final Score: '+str(loss.iloc[-1]))
+        final_scores.append(loss.iloc[-1])
+
+    ax[1].legend()
+
+    ax[0].xaxis.set_ticks(np.arange(1, ax[0].get_xlim()[1], cycle_iter_length))
+    ax[1].xaxis.set_ticks(np.arange(1, ax[0].get_xlim()[1], cycle_iter_length))
+
+    plt.show()
+    return final_scores
+
+def plot_scores(starting_scores:pd.DataFrame, starting_score:float, scored_runs:list, final_scores:list, title:str):
+    fig, ax = plt.subplots(1, len(scored_runs)+1, figsize=(25, 8))
+    fig.suptitle(title)
+    palette = itertools.cycle(seaborn.color_palette())
+    
+    seaborn.regplot(data=starting_scores, x='Reference', y='Calculated', label='FF', fit_reg=True, ax=ax[0], color='gray')
+    ax[0].set_ylim(top=5000)
+    ax[0].set_xlim(xmax = 5000)
+    ax[0].set_title('INITIAL - '+str(starting_score))
+    
+    for i, run in enumerate(scored_runs):
+        color = next(palette)
+        seaborn.regplot(data=run, x='Reference', y='Calculated', label='FF', fit_reg=True, ax=ax[i+1], color=color)
+    
+        ax[i+1].set_ylim(top=5000)
+        ax[i+1].set_xlim(xmax = 5000)
+        ax[i+1].set_title('Score: '+str(final_scores[i]))
+    
+    plt.show()
+
+def plot_off_diag_scatter(starting_scores:pd.DataFrame, starting_score:float, scored_runs:list, final_scores:list, title:str=''):
+    fig, ax = plt.subplots(1, 4, figsize=(32, 10))
+    fig.suptitle('Off-Diagonal Eigenmatrix terms'+title)
+    palette = itertools.cycle(seaborn.color_palette())
+
+    off_diag_start = starting_scores.loc[starting_scores['Reference'] == 0.0000]
+    off_diag_start = off_diag_start.sort_values(by='Calculated', ignore_index=True)
+    seaborn.regplot(data=off_diag_start, x=off_diag_start.index, label='FF', y='Calculated', fit_reg=False, ax=ax[0], color='gray')
+    ax[0].set_title('FUERZA - '+str(starting_score))
+    max_y = max(off_diag_start['Calculated'])
+
+    for i, run in enumerate(scored_runs):
+        off_diag = run.loc[run['Reference'] == 0.0000]
+        off_diag = off_diag.sort_values(by='Calculated', ignore_index=True)
+
+        color = next(palette)
+        seaborn.regplot(data=off_diag, x=off_diag.index, label='FF', y='Calculated', fit_reg=False, ax=ax[i+1], color=color)
+        ax[i+1].set_title('Score: '+str(final_scores[i]))
+        ax[i+1].set_ylim(top=max_y, bottom=-max_y)
+
+    plt.show()
+
+def plot_off_diag_violin(starting_scores:pd.DataFrame, starting_score:float, scored_runs:list, final_scores:list, title:str=''):
+
+    fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+    off_diag_start = starting_scores.loc[starting_scores['Reference'] == 0.0000]
+    off_diag_start = off_diag_start.sort_values(by='Calculated', ignore_index=True)
+
+    off_diag_merged = pd.concat([run.loc[run['Reference'] == 0.0000] for run in scored_runs])
+    off_diag_merged = pd.concat([off_diag_start, off_diag_merged])
+    off_diag_merged['FF'] = off_diag_merged['FF'].astype(str)
+
+    seaborn.violinplot(data=off_diag_merged, x='FF', y='Calculated', title='Off-Diagonal Eigenmatrix terms'+title)
+    ax.set_label('FF Score')
+
+    plt.show()
+
+def plot_fit_diag_scores(starting_scores:pd.DataFrame, starting_score:float, scored_runs:list, final_scores, title:str=''):
+    # Plot Diagonal Elements with a linear fit
+
+    fig, ax = plt.subplots(1, 4, figsize=(32,8))
+    fig.suptitle('Diagonal Eigenmatrix terms after a PSO only - Rh Hyd Enamides')
+    palette = itertools.cycle(seaborn.color_palette())
+
+    diag = starting_scores.loc[starting_scores['Reference'] != 0.0000]
+    slope, intercept, r2, pv, se = stats.linregress(diag['Reference'], diag['Calculated'])
+
+    seaborn.regplot(data=diag, y='Calculated', x='Reference', color=next(palette), label='FF', line_kws={'label':'$y=%3.7s*x+%3.7s   r2:%3.7s$'%(slope, intercept, r2)}, ax=ax[0])
+    ax[0].legend()
+    ax[0].set_title('FUERZA - '+str(starting_score))
+
+    for i, run in enumerate(scored_runs):
+        diag = run.loc[run['Reference'] != 0.0000]
+        slope, intercept, r2, pv, se = stats.linregress(diag['Reference'], diag['Calculated'])
+
+        seaborn.regplot(data=diag, y='Calculated', x='Reference', color=next(palette), label=final_scores[i], line_kws={'label':'$y=%3.7s*x+%3.7s   r2:%3.7s$'%(slope, intercept, r2)}, ax=ax[i+1])
+
+        ax[i+1].legend()
+        ax[i+1].set_title('Score: '+str(final_scores[i]))
+
+    for a in ax:
+        seaborn.lineplot(data=diag, x='Reference', y='Reference', color='gray', ax=a)
+
+def linear_fit_diag_scores(starting_scores:pd.DataFrame, starting_score:float, scored_runs:list, final_scores, title:str=''):
+    # Plot Diagonal Elements with a linear fit
+
+    fig, ax = plt.subplots(1, 4, figsize=(32,8))
+    fig.suptitle('Diagonal Eigenmatrix terms after a PSO only - Rh Hyd Enamides')
+    palette = itertools.cycle(seaborn.color_palette())
+
+    diag = starting_scores.loc[starting_scores['Reference'] != 0.0000]
+    slope, intercept, r2, pv, se = stats.linregress(diag['Reference'], diag['Calculated'])
+
+    seaborn.regplot(data=diag, y='Calculated', x='Reference', color=next(palette), label='FF', line_kws={'label':'$y=%3.7s*x+%3.7s   r2:%3.7s$'%(slope, intercept, r2)}, ax=ax[0])
+    ax[0].legend()
+    r2_ = r2_score(diag['Reference'], diag['Calculated'])
+    ax[0].set_title('FUERZA - '+str(starting_score)+' y=x r2:'+str(r2_))
+
+
+    for i, run in enumerate(scored_runs):
+        diag = run.loc[run['Reference'] != 0.0000]
+        slope, intercept, r2, pv, se = stats.linregress(diag['Reference'], diag['Calculated'])
+
+        seaborn.regplot(data=diag, y='Calculated', x='Reference', color=next(palette), label=final_scores[i], line_kws={'label':'$y=%3.7s*x+%3.7s   r2:%3.7s$'%(slope, intercept, r2)}, ax=ax[i+1])
+        r2_ = r2_score(diag['Reference'], diag['Calculated'])
+        ax[i+1].legend()
+        ax[i+1].set_title('Score: '+str(final_scores[i])+' y=x r2:'+str(r2_))
+
+    for a in ax:
+        seaborn.lineplot(data=diag, x='Reference', y='Reference', color='gray', ax=a)
+
+def get_ff_params(base_direc:str, directories:list, final_scores:list, bond_rows:list, angle_rows:list, title:str=''):# -> tuple[list, list]:
+    # Plot FCs
+
+    bonds = []
+    angles = []
+
+    for directory, score in zip(directories, final_scores):
+        bonds.append(pd.read_csv(os.path.join(base_direc, directory, 'rh_qf_pso_end.fld'), skiprows=lambda x: x not in bond_rows, delim_whitespace=True, names=bond_cols).assign(FF=score))
+        angles.append(pd.read_csv(os.path.join(base_direc, directory, 'rh_qf_pso_end.fld'), skiprows=lambda x: x not in angle_rows, delim_whitespace=True, names=angle_cols).assign(FF=score))
+
+    return bonds, angles
+
+def plot_ff_params(start_bonds:pd.DataFrame, start_angles:pd.DataFrame, bonds:list, angles:list, final_scores:list, title:str=''):
+    fig, ax = plt.subplots(1, 2, figsize=(24, 8))
+    fig.suptitle('Force Constants'+title)
+    ax[0].set_title('Bonds')
+    ax[1].set_title('Angles')
+
+    palette = itertools.cycle(seaborn.color_palette())
+
+    color = next(palette)
+    seaborn.regplot(data=start_bonds, label='FUERZA', x = start_bonds.index, y="Force Constant", fit_reg=False, ax=ax[0], color=color)
+    seaborn.regplot(data=start_angles, label='FUERZA', x = start_angles.index, y="Force Constant", fit_reg=False, ax=ax[1], color=color)
+    bond_labels = bonds[0][['atom1', 'atom2', 'param_type']].values
+    bond_labels = [str(bl) for bl in bond_labels]
+    angles_labels = angles[0][['atom1', 'atom2', 'atom3', 'param_type']].values
+    angles_labels = [str(al) for al in angles_labels]
+    print(bond_labels)
+    for i in range(len(bonds)):
+        color = next(palette)
+        seaborn.regplot(data=bonds[i], label=final_scores[i], x = bond_labels, y="Force Constant", fit_reg=False, ax=ax[0], color=color)
+        seaborn.regplot(data=angles[i], label=final_scores[i], x = angles_labels, y="Force Constant", fit_reg=False, ax=ax[1], color=color)
+
+    plt.xticks(rotation=45)
+    ax[1].legend()
+    ax[0].set_ylabel('Force Constant (mdyne/Ang)')
+    ax[1].set_ylabel('Force Constant (mdyne/Ang)')
+    ax[0].set_xlabel('atom1, atom2, param_type')
+    ax[1].set_xlabel('atom1, atom2, atom3, param_type')
+    plt.show()
+
+
+
