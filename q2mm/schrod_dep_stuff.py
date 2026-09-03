378012e6fb3d0b04fc3f584a136608932b3b3699
mmfarrugia <mfarrugi@nd.edu>
2026-09-03 14:39:42 -0400

    FIX: integer number of licenses, not inf

--- q2mm/schrod_dep_stuff.py
+++ q2mm/schrod_dep_stuff.py
@@ -51,8 +51,8 @@ logger = logging.getLogger(__file__)
 
 
 def check_licenses():
-    if not schrod_on: return (math.inf, math.inf) 
-    # hacky way to parallelize amber without having to extricate schrodinger from 
+    if not schrod_on: return (100, 100) 
+    # TODO MF hacky way to parallelize amber without having to extricate schrodinger from 
     # swarm optimizer bc it's only in there bc it had to be unless all q2mm refactored... NOT RECOMMENDED
     # this is NOT how it will work in the future
     max_fails=5
